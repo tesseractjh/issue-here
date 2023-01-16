1eb9faf8bb982d51ab6b7c89bac5ec08a65f5042
Tesseract <tensai3355@gmail.com>
2023-01-16 21:39:32 +0900

    :wheelchair: 잘못된 aria 어트리뷰트 수정
    - aria-haspopup과 aria-expanded를 혼동하였음. aria-haspopup을 true로 고정하고 aria-expanded에는 Filter의 개폐 여부를 넣음

--- src/components/common/Filter/FilterTrigger.tsx
+++ src/components/common/Filter/FilterTrigger.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
-import { ReactComponent as ChevronIcon } from '@assets/icons/chevron.svg';
 import Button from '@components/common/Button';
+import { ReactComponent as ChevronIcon } from '@assets/icons/chevron.svg';
 import pxToRem from '@utils/pxToRem';
 import useFilterContext from './hooks/useFilterContext';
 
@@ -28,7 +28,8 @@ function FilterTrigger({ content }: Props) {
       elementType="button"
       theme="normal"
       onClick={handleClick}
-      aria-haspopup={isOpen}
+      aria-haspopup
+      aria-expanded={isOpen}
       aria-controls={`popup-${id}`}
     >
       <Content>
