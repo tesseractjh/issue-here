c9339086b28069d799c79f138507dd734f45e5da
Tesseract <tensai3355@gmail.com>
2023-01-17 18:40:24 +0900

    :art: localStorageEffect를 다른 atom에서도 사용할 수 있도록 추상화

--- src/recoil/theme/atoms.ts
+++ src/recoil/theme/atoms.ts
@@ -1,22 +1,12 @@
-import type { AtomEffect } from 'recoil';
 import { atom } from 'recoil';
+import { localStorageEffect } from '@utils/effect';
 
 const getTheme = () => (localStorage.getItem('theme') ?? 'light') as Theme;
-
-const localStorageEffect =
-  (key: string): AtomEffect<boolean> =>
-  ({ onSet }) => {
-    onSet((newValue, _, isReset) => {
-      if (isReset) {
-        localStorage.removeItem(key);
-      } else {
-        localStorage.setItem(key, newValue ? 'dark' : 'light');
-      }
-    });
-  };
 
 export const themeState = atom<boolean>({
   key: 'themeState',
   default: getTheme() === 'dark',
-  effects: [localStorageEffect('theme')]
+  effects: [
+    localStorageEffect('theme', (newValue) => (newValue ? 'dark' : 'light'))
+  ]
 });
